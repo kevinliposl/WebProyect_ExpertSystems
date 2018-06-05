3385db611f7905649c2ec1a0a7c84bbe9f66348b
kevinliposl <kevinliposl@gmail.com>
2018-06-04 18:47:57 -0600

    asd

--- controller/AppUserController.php
+++ controller/AppUserController.php
@@ -1,12 +1,11 @@
 <?php
-
 /**
  * @author <kevin.sandoval@ucr.ac.cr><kevin.sandoval@ucr.ac.cr>
  * @version 1.0
  * @copyright (c) 2018, Kevin Sandoval Loaiza b46549, Pablo Barrientos Brenes b40920
  * @access public
  * @category controller
- * Class UserController     
+ * Class AppUserController     
  */
 class AppUserController {
 
@@ -19,18 +18,18 @@ class AppUserController {
     }
 
     /**
+     * 
+     * ?controller=AppUser&action=signIn
      * Funcion para iniciar sesión 
      */
-    function signIn() { 
+    function signIn() {
         if (isset($_REQUEST['mail']) && isset($_REQUEST['password'])) {
             $model = new UserModel;
             $result = $model->signIn($_REQUEST['mail'], $_REQUEST['password']);
-            echo json_encode(array(
-                'User' => array('mail' => $_REQUEST['mail'], 'password' => $_REQUEST['password']),
-                'result' => $result['result'],
-            ));
+            echo json_encode(array('result' => $result['result']));
         } else {
             echo json_encode(array('result' => 0));
         }
     }
+
 }

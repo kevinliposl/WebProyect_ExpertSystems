99505351d61a5633a0ebaf4e3278c67d705abed5
kevinliposl <kevinliposl@gmail.com>
2018-06-06 14:51:54 -0600

    awd

--- controller/AppDestinyController.php
+++ controller/AppDestinyController.php
@@ -18,11 +18,11 @@ class AppDestinyController {
     function __construct() {
         $this->view = new View();
         require_once 'model/DestinyModel.php';
+
+        $this->distance = 0;
+        $this->recommendation = array();
     }
 
-    /**
-     * Funcion que calcula la distancia entre 2 vectores del mismo tamaño, con sus caracteristicas ordenadas. 
-     */
     function distanceEuclidean($arrayA, $arrayB) {
         if (count($arrayA) !== count($arrayB)) {
             return NULL;
@@ -31,28 +31,36 @@ class AppDestinyController {
         $length = count($arrayA);
 
         for ($i = 0; $i < $length; $i++) {
-            $distance += pow(($arrayA[$i] - $arrayB[$i]), 2);
+            if (is_string($arrayA[$i])) {
+                $distance += levenshtein($arrayA[$i], $arrayB[$i]);
+            } else {
+                $distance += pow(($arrayA[$i] - $arrayB[$i]), 2);
+            }
         }
         return 1 / ( 1 + sqrt((float) $distance));
     }
 
     /**
+     * http://localhost/WebProject_ExpertSystems_B49020_B46549/?controller=AppDestiny&action=basicSearch&location=Puntarenas&attraction=1&type=2&stars=2
+     * 
      * Funcion para busqueda basica 
      */
     function basicSearch() {
-        if (isset($_REQUEST['location']) && isset($_REQUEST['attraction']) && isset($_REQUEST['type']) && isset($_REQUEST['qualification'])) {
+        if (isset($_REQUEST['location']) && isset($_REQUEST['attraction']) && isset($_REQUEST['type']) && isset($_REQUEST['stars'])) {
             $model = new DestinyModel;
-            $result = $model->selectAll();
-            $arrayA = array($_POST['ca'], $_POST['ec'], $_POST['ea'], $_POST['or']);
+            $vars = $model->selectAll();
+            $arrayA = array($_REQUEST['location'], $_REQUEST['attraction'], $_REQUEST['type'], $_REQUEST['stars']);
             foreach ($vars as $var) {
-                $arrayB = array($var['ca'], $var['ec'], $var['ea'], $var['or']);
+                $arrayB = array($var['location'], $var['attraction'], $var['type'], $var['stars']);
                 $tmp = $this->distanceEuclidean($arrayA, $arrayB);
                 if ($tmp > $this->distance) {
                     $this->distance = $tmp;
-                    $this->tmp = $var['name'];
+                    if (count($this->recommendation) <= 5) {
+                        array_push($this->recommendation, $var);
+                    }
                 }
             }
-            echo json_encode(array('result' => $result));
+            echo json_encode($this->recommendation);
         } else {
             echo json_encode(array('result' => 0));
         }
@@ -71,5 +79,4 @@ class AppDestinyController {
             echo json_encode(array('result' => 0));
         }
     }
-
 }
